8abd91b9a6f891ec74ce5f71360fd2f6697651ab
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-30 10:28:47 +0300

    chore: to classes

--- src/.helpers/emitEvent/index.ts
+++
@@ -1,7 +0,0 @@
-export function emitEvent<EventMap, Type extends keyof EventMap & string>(
-  eventTarget: EventTarget,
-  type: Type,
-  detail: EventMap[Type]
-): boolean {
-  return eventTarget.dispatchEvent(new CustomEvent(type, { detail }))
-}
--- src/.helpers/index.ts
+++ src/.helpers/index.ts
@@ -1,11 +1,1 @@
-export function intersects(a: HTMLElement, b: HTMLElement): boolean {
-  const ar = a.getBoundingClientRect()
-  const br = b.getBoundingClientRect()
-
-  return !(
-    ar.right < br.left ||
-    ar.left > br.right ||
-    ar.bottom < br.top ||
-    ar.top > br.bottom
-  )
-}
+export { intersects } from './intersects/index.js'
